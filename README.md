b4a0409854810ea6d0899ec76e202754829dd81a
Arjun Nahar <ArjunNahar@dunsterhouse.co.uk>
2023-08-18 11:38:33 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Make an assets folder inside the 360_image_render folder
+
+The js is already set up for the dunsterhouse image format 
+
+change it in the script.js file to whatever format you have for your images
---
+++ script.js
@@ -0,0 +1,32 @@
+const card = document.getElementById("card");
+
+
+
+for(let i = 1; i <= 50; i++){
+
+  const dynamicImage = document.createElement("img");
+  const dynamicSpan = document.createElement("span"); 
+
+
+  var img_no = i*2
+  var val;
+  if(img_no < 10){
+    val = "000"+img_no.toString() 
+  }
+  else if( img_no >= 10){
+    val = "00"+img_no.toString() 
+  }
+  else if( img_no >= 100){
+    val = "0"+img_no.toString() 
+  }
+
+  dynamicSpan.style = "--i:"+i.toString()+";"
+
+  dynamicImage.src = "./assets/addroom_modular_3030_door_dwarf_panel_"+val+".jpg"
+  card.appendChild(dynamicSpan)
+  card.appendChild(dynamicImage)
+
+  // card += `<span style="--i:'+i.toString()+';"></span>
+  // <img src="../assets/addroom_modular_3030_door_dwarf_panel_'+val+'.jpg">`
+}
+
